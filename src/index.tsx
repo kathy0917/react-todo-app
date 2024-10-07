4b1f5287f2b01d15599fbe5e1b62622f8f064959
Nahyeon Kang <kathy95917@naver.com>
2024-10-07 16:59:42 +0900

    feat: 프로젝트 초기설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { darkTheme } from './theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2E363F',
+  textColor: 'white',
+  accentColor: '#8784C9',
+};
